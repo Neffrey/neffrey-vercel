a254260f16cb00f1ee136e60cf7c6dd4d6416f1c
Neffrey <neffreyl@gmail.com>
2022-09-17 08:27:40 -0500

    auth fix?

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,12 +5,24 @@ import { env } from "../../../env/server.mjs";
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
+    async session({ session, token }) {
+      // Persist the OAuth id_token to the session token
+      session.id_token = token.id_token;
       return session;
     },
+    async jwt({ token, account }) {
+      // Persist the OAuth id_token to the jwt token right after signin
+      if (account && account.id_token) {
+        token.id_token = account.id_token;
+      }
+      return token;
+    },
+    // session({ session, user }) {
+    //   if (session.user) {
+    //     session.user.id = user.id;
+    //   }
+    //   return session;
+    // },
   },
   // Configure one or more authentication providers
   providers: [
@@ -26,6 +38,10 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  session: { strategy: "jwt" },
+  theme: {
+    colorScheme: "light",
+  },
 };
 
 export default NextAuth(authOptions);
